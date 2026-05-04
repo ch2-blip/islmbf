b271de7fa20ec2570580757c04f7022ee1ba41af
baitianruo <156179288+baitianruo@users.noreply.github.com>
2026-05-04 12:32:29 +0800

    Publish application

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import legacy from "@vitejs/plugin-legacy"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: [
+        "Android >= 6",
+        "Chrome >= 60",
+        "iOS >= 11",
+        "Safari >= 11",
+        "Edge >= 79",
+        "Firefox >= 60",
+        "> 0.3%",
+        "not dead",
+      ],
+      modernPolyfills: true,
+      renderLegacyChunks: true,
+      polyfills: ["es.promise", "es.array.iterator", "es.object.assign", "es.symbol"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    cssTarget: ["chrome60", "safari11"],
   },
 })
